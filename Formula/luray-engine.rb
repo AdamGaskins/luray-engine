d23201adf2582c0436aab1ad3dccd242774de22d
Adam Gaskins <adam@theadamgaskins.com>
2026-07-26 21:05:43 +0900

    (chore) Update homebrew tap

--- Formula/luray-engine.rb
+++ Formula/luray-engine.rb
@@ -1,12 +1,12 @@
 class LurayEngine < Formula
   desc "The fastest way to prototype a raylib game!"
   homepage "https://github.com/AdamGaskins/luray-engine"
-  version "0.1.0-alpha.3"
+  version "0.1.0-alpha.4"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/AdamGaskins/luray-engine/releases/download/0.1.0-alpha.3/luray-0.1.0-alpha.3-macos-arm.tar.gz"
-    sha256 "17bdb90bc19624369a38c3e63e0c22e96d7746ee2766c5eeeb13a4816726e45f"
+    url "https://github.com/AdamGaskins/luray-engine/releases/download/0.1.0-alpha.4/luray-0.1.0-alpha.4-macos-arm.tar.gz"
+    sha256 "cd8007e2938faf4016fafedc7a82f2d381b712e27b6134e2e47071fa253707d7"
   else
     odie "luray: no prebuilt binary for this platform yet (supported: macOS arm)"
   end
